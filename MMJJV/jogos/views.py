2608d8f0c2474d9111f0e6f6b3a33d14fc0a0aac
João Victor Nobre <137822615+ZacWingrosja@users.noreply.github.com>
2024-07-03 00:48:19 -0300

    Delete MMJJV directory

--- MMJJV/jogos/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from .models import Jogo
-def lista_jogos(request):
-    jogos = Jogo.objects.all()
-    return render(request, 'jogos/index.html', {'jogos':jogos})
