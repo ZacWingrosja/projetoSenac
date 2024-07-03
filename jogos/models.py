0e3ca364eb86584a8a42d0e504221bebde006c66
João Victor Nobre <137822615+ZacWingrosja@users.noreply.github.com>
2024-07-03 00:49:16 -0300

    versao 3

--- jogos/models.py
+++ jogos/models.py
@@ -8,6 +8,7 @@ class Jogo(models.Model):
     lancamento = models.DateField()
     plataforma = models.CharField(max_length=50)
     genero = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='template/assets/imagens', null=True, blank=True)
 
     def __str__(self):
         return self.nome
--- setup/settings.py
+++ setup/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'template/assets'),
 ]
 
 
